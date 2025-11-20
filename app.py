b2a08ca0dfa00a6a388771b9c33bcaa62eccb004
OmarJB20 <ojc.santillan@yavirac.edu.ec>
2025-11-20 12:00:58 -0500

    Puertos

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def home():
     return "Aplicación desplegada con Docker  funcionando correctamente - Saludos a todos !"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=6000)
