98ae5e93d10a35bbb654b78f46724a8269219b92
OmarJB20 <ojc.santillan@yavirac.edu.ec>
2025-11-20 11:44:33 -0500

    Cambio de Mensaje

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Aplicación desplegada con Docker  funcionando correctamente!"
+    return "Aplicación desplegada con Docker  funcionando correctamente - Saludos a todos !"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
